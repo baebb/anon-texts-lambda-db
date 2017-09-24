4caf67d9866b08de4ce4a84df54f9d106aee9f5a
Ross Dyson <dyson.ross@gmail.com>
2017-09-24 22:27:20 +1000

    testing shit

--- sentMessages/get.js
+++ sentMessages/get.js
@@ -5,6 +5,7 @@ const dynamoDB = new AWS.DynamoDB.DocumentClient();
 module.exports.handler = (event, context, callback) => {
   // console.log('event: ',event);
   // convert stuff to json if needed
+  console.log(process.env.stage);
   let eventData;
   try {
     eventData = JSON.parse(event);
